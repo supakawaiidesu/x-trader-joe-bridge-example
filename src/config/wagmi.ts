4dc64dc784be39941b2aaf5c1c58f8d4b92eb294
Krunal Amin <kishanamin97@gmail.com>
2024-02-28 15:32:48 -0700

    fix base

--- src/config/wagmi.ts
+++ src/config/wagmi.ts
@@ -5,9 +5,56 @@ import { fantom, arbitrum, optimism, bsc, avalanche, mainnet } from "@wagmi/core
 
 import { createWagmiAdapter } from "@layerzerolabs/x-trader-joe-bridge";
 
+const base = {
+  id: 8453,
+  name: "Base Chain",
+  network: "base",
+  nativeCurrency: {
+    name: "Ethereum", 
+    symbol: "ETH", 
+    decimals: 18 
+  },
+  rpcUrls: {
+    default: {
+      http: ["https://base.llamarpc.com	"] 
+    }
+  },
+  blockExplorers: {
+    default: {
+      name: "Base Explorer",
+      url: "https://basescan.org/"
+    }
+  }
+};
+
+/*
+const mainnet = {
+  id: 1,
+  name: "Ethereum",
+  network: "mainnet",
+  nativeCurrency: {
+    name: "Ethereum", 
+    symbol: "ETH", 
+    decimals: 18 
+  },
+  rpcUrls: {
+    default: {
+      http: ["https://eth.llamarpc.com"] 
+    }
+  },
+  blockExplorers: {
+    default: {
+      name: "Base Explorer",
+      url: "https://etherscan.org/"
+    }
+  }
+};
+*/
+
+
 const { chains: wagmiChains, provider } = configureChains(
   // provide wagmi chain configuration
-  [fantom, optimism, arbitrum, bsc, avalanche, mainnet],
+  [fantom, optimism, arbitrum, bsc, avalanche, mainnet, base],
   [publicProvider()]
 );
 
